96ada074327da0373657ac362f7c0e39e977cada
KITH <kimcheng_kith@hotmail.com>
2018-09-11 10:05:06 +0200

    j'ai modifié le code pour trouver le nombre secret entre 1 et 1000 en 10 essais

--- main.c
+++ main.c
@@ -7,15 +7,27 @@
 * en disant juste si le le nombre est plus grand ou plus petit
 */
 
+int rand_a_b(int, int);
 int main()
-{   int nombre_aleatoire = 0;
-    int a=1;
-    int b=10;
+{   int nombre_aleatoire = 0, halfNb;
+    int a=1, i=0, nbSecret;
+    int b=1000;
+    char reponse;
     srand(time(NULL)); // initialisation de rand
     nombre_aleatoire = rand_a_b(a,b);
-    printf("%d ",nombre_aleatoire);
+    printf("Le nombre aléatoire généré par le programme est : %d \n",nombre_aleatoire);
 
-
+    while(i < 10) {
+        halfNb = (a+b)/2;
+        printf("Le nombre est il > %d \n", halfNb);
+        scanf(" %c", &reponse);
+        if (reponse == 'o')   a = halfNb + 1;
+        else b = halfNb;
+        i++;
+    }
+    if (reponse == 'o') nbSecret = b;
+    else nbSecret = a;
+    printf("Le nombre secret qu'on a trouvé est : %d \n", nbSecret);
 
     return 0;
 }
